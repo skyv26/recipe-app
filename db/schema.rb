daeb7cb007490acf422f0f0258b65a4daa411058
skyv26 <dev.aakashv@gmail.com>
2023-01-05 15:57:27 +0530

    create `inventory` model and generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_102636) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_102713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_102636) do
     t.string "name"
     t.string "measurement_unit"
     t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
