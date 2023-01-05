0d0462d8b2053a2f6c98ac0aa33d3bd0f9e78941
skyv26 <dev.aakashv@gmail.com>
2023-01-05 15:55:49 +0530

    create `food` model and generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_102427) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_102529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity", limit: 2
