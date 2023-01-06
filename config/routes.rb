01ce72c4f6409e0364e1cff29402505e44b514ad
skyv26 <dev.aakashv@gmail.com>
2023-01-06 23:56:24 +0530

    Add the `route` and `view` for recipe_food

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
     resources :recipes
     resources :inventorys
   end
+
+  resources :recipes, :foods do
+    resources :recipe_foods
+  end
+
   resources :foods, :inventorys do
     resources :inventory_foods
   end
@@ -15,5 +20,6 @@ Rails.application.routes.draw do
   resources :inventorys
   resources :foods
   resources :inventory_foods
+  resources :recipe_foods
 
 end
