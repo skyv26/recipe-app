35975139f3ab3c30de3b3f4596d5d195e858b80f
skyv26 <dev.aakashv@gmail.com>
2023-01-06 21:42:53 +0530

    Resolve the `rubocop` linter errors

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -2,5 +2,5 @@ class InventoryFood < ApplicationRecord
   belongs_to :inventory
   belongs_to :food
 
-  validates :quantity, presence: true, length: {in 1..32000}
+  validates :quantity, presence: true, length: { in: 1..32_000 }
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,5 +2,5 @@ class RecipeFood < ApplicationRecord
   belongs_to :food
   belongs_to :recipe
 
-  validates :quantity, presence: true, length: {in 1..32000}
+  validates :quantity, presence: true, length: { in: 1..32_000 }
 end
