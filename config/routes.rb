8d9ca0f0757b5336dfb0fb86a39f4ea44bea2852
skyv26 <dev.aakashv@gmail.com>
2023-01-07 00:09:36 +0530

    Add the `route` and `view` for public_recipe

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "public_recipes#index"
   resources :users do
     resources :recipes
     resources :inventorys
