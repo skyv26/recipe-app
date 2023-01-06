65b0b386baa975be032e55a8eed31fcef92f7f11
skyv26 <dev.aakashv@gmail.com>
2023-01-06 21:12:58 +0530

    Add the `user-validation`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :inventories, dependent: :destroy
+
+  validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_112329) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "role"
+    t.string "role", default: "guest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
